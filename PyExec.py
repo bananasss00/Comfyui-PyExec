90ac8537408ab60c20d7c91b8c222cfd0ea9bd33
bananasss00 <bananasss00@yandex.ru>
2024-08-22 10:29:38 +0300

    init

---
+++ PyExec.py
@@ -0,0 +1,83 @@
+import io
+import contextlib
+
+CATEGORY = "SP-Nodes"
+
+class AnyType(str):
+    def __ne__(self, __value: object) -> bool:
+        return False
+    
+ANY_TYPE = AnyType("*")
+
+class PyExec:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "py": ("STRING", {"default": '', "multiline": True}),
+            },
+            "optional": {
+                "a1": (ANY_TYPE,),
+                "a2": (ANY_TYPE,),
+                "a3": (ANY_TYPE,),
+                "a4": (ANY_TYPE,),
+                "a5": (ANY_TYPE,),
+            },
+        }
+
+    RETURN_TYPES = (ANY_TYPE, ANY_TYPE, ANY_TYPE, ANY_TYPE, ANY_TYPE)
+    RETURN_NAMES = ('r1', 'r2', 'r3', 'r4', 'r5')
+    OUTPUT_IS_LIST = (True,True,True,True,True)
+    FUNCTION = "doit"
+    CATEGORY = CATEGORY
+    OUTPUT_NODE = False
+
+    def doit(s, py, a1=None, a2=None, a3=None, a4=None, a5=None):
+        try:
+            output = io.StringIO()
+            local_vars = {
+                'a1': a1,
+                'a2': a2,
+                'a3': a3,
+                'a4': a4,
+                'a5': a5,
+                'r1': None,
+                'r2': None,
+                'r3': None,
+                'r4': None,
+                'r5': None
+            }
+            
+            # Создаем объединение глобальных и локальных переменных
+            exec_globals = globals().copy()
+            exec_globals.update(local_vars)
+        
+            with contextlib.redirect_stdout(output):
+                exec(py, exec_globals, exec_globals)
+            
+            def to_list(value):
+                return value if isinstance(value, list) else [value]
+            
+            result = (
+                to_list(exec_globals.get('r1', None)),
+                to_list(exec_globals.get('r2', None)),
+                to_list(exec_globals.get('r3', None)),
+                to_list(exec_globals.get('r4', None)),
+                to_list(exec_globals.get('r5', None))
+            )
+
+            captured_output = output.getvalue()
+            print(f'PyExec: {captured_output}')
+
+            return result
+        
+        except Exception as e:
+            import traceback
+            stacktrace = traceback.format_exc()
+            err = f"Произошла ошибка: {e}]\n{stacktrace}"
+            print(err)
+            return ([err],) * 5
+        
+NODE_CLASS_MAPPINGS = {
+    "PyExec": PyExec, 
+}
---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+@author: SeniorPioner
+@title: PyExec
+@nickname: PyExec
+@description: Comfyui runtime python code execution
+"""
+
+from PyExec import NODE_CLASS_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS']
