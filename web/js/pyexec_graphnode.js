c2f357ca681e16b79d4e49da97b846a3aa843407
rdp <rdp>
2024-10-21 15:35:04 +0300

    code generator for group nodes

--- web/js/pyexec_graphnode.js
+++ web/js/pyexec_graphnode.js
@@ -9,44 +9,230 @@ const addMenuHandler = (nodeType, cb)=> {
 	};
 }
 
-const copyGraphNodes = function (nodes) {
+function toVar(str) {
+    return str
+    .trim() // Удаляем пробелы в начале и конце
+    .toLowerCase() // Преобразуем все символы в нижний регистр
+    .replace(/[^a-z0-9\s]/g, '') // Удаляем все символы, кроме букв, цифр и пробелов
+    .replace(/\s+/g, '_'); // Заменяем одно или несколько пробелов на подчеркивание
+}
+
+// function toVar(title) {
+//     return title
+//       // Убираем все символы, кроме букв, цифр и пробелов
+//       .replace(/[^a-zA-Z0-9\s]/g, '')
+//       // Разбиваем на слова по пробелам
+//       .trim()
+//       .split(/\s+/)
+//       // Преобразуем в camelCase
+//       .map((word, index) =>
+//         index === 0
+//           ? word.toLowerCase() // Первое слово с маленькой буквы
+//           : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() // Остальные с большой буквы
+//       )
+//       .join('');
+// }
+
+const copyGraphNodes = (nodes) => {
+    const names = new Set();
     const code = [];
+    const vars = {};
 
-    for (let i in nodes) {
-        const node = nodes[i];
-        const nodeType = node.constructor.type;
-        console.log(node);
+    const makeUniqueName = (name) => {
+        name = toVar(name);
+        let uniqueName = name;
+        let counter = 1;
+
+        while (names.has(uniqueName)) {
+            uniqueName = `${name}_${counter++}`;
+        }
+
+        names.add(uniqueName);
+
+        return uniqueName;
+    };
+
+    function topologicalSort(nodes) {
+        const graph = new Map();
+        const inDegree = new Map();
         
-        const args = [];
-        args.push(`'${nodeType}'`);
-
-        for (let j in node.inputs) {
-            const input = node.inputs[j];
-            args.push(`${input.name}=TYPE_${input.type}`);
-        }
-
-        for (let j in node.widgets) {
-            const widget = node.widgets[j];
-            if (widget.type.startsWith('converted-')) {
-                continue;
+        // Инициализация графа и степени входа для каждого узла
+        nodes.forEach(node => {
+            graph.set(node, []);
+            inDegree.set(node, 0);
+        });
+        
+        // Построение графа и подсчет входящих ребер
+        nodes.forEach(node => {
+            const outputs = node.outputs;
+            const inputs = node.inputs;
+    
+            if (outputs) {
+                Object.values(outputs).forEach(output => {
+                    output.links.forEach(outId => {
+                        nodes.forEach(n => {
+                            Object.values(n.inputs).forEach(input => {
+                                if (input.link === outId) {
+                                    graph.get(node).push(n);
+                                    inDegree.set(n, inDegree.get(n) + 1);
+                                }
+                            });
+                        });
+                    });
+                });
             }
-            const value = typeof widget.value === 'string' ? `"${widget.value}"` : widget.value;
-            args.push(`${widget.name}=${value}`);
+        });
+    
+        // Поиск узлов с нулевой степенью входа
+        const stack = [];
+        inDegree.forEach((degree, node) => {
+            if (degree === 0) {
+                stack.push(node);
+            }
+        });
+    
+        const sortedList = [];
+        
+        // Топологическая сортировка
+        while (stack.length > 0) {
+            const current = stack.pop();
+            sortedList.push(current);
+    
+            graph.get(current).forEach(neighbor => {
+                inDegree.set(neighbor, inDegree.get(neighbor) - 1);
+                if (inDegree.get(neighbor) === 0) {
+                    stack.push(neighbor);
+                }
+            });
         }
-
-        const outputs = [];
-        for (let j in node.outputs) {
-            const output = node.outputs[j];
-            outputs.push(`${j}=${output.name}(${output.type})`);
-        }
-
-        code.push(`graph.node(${args.join(', ')}); # out: ${outputs.join(',')}`)
+    
+        return sortedList;
     }
 
-    navigator.clipboard.writeText(code.join('\n'))
-        .catch((err) => {
-            console.error('Error: ', err);
+
+    console.log(nodes);
+
+    let nodeObjs = [];
+    const outLinks = {};
+
+    for (const i in nodes) {
+        const node = nodes[i];
+        const { type: nodeType } = node.constructor;
+        const args = [`'${nodeType}'`];
+        
+        const nodeObj = {};
+        nodeObj.title = node.title;
+        nodeObj.var = makeUniqueName(nodeObj.title);
+        nodeObj.type = nodeType;
+        nodeObj.inputs = {};
+        nodeObj.widgets = {};
+        nodeObj.outputs = {};
+
+        node.inputs.forEach(input => {
+            const o = nodeObj.inputs[input.name] = {};
+            o.name = input.name;
+            o.label = input.label;
+            o.var = makeUniqueName(o.label ?? o.name);
+            o.type = input.type;
+            o.link = input.link;
         });
+
+        node.widgets?.forEach(widget => {
+            if (widget.type.startsWith('converted-'))
+                return;
+            const o = nodeObj.widgets[widget.name] = {};
+            o.name = widget.name;
+            o.var = makeUniqueName(o.name);
+            o.type = widget.type;
+            o.value = widget.value;
+        });
+        
+        node.outputs.forEach(output => {
+            const o = nodeObj.outputs[output.name] = {};
+            o.name = output.name;
+            o.label = output.label;
+            o.var = makeUniqueName(o.label ?? o.name);
+            o.type = output.type;
+            o.links = [];
+            for (let j in output.links) {
+                const link = output.links[j];
+                o.links.push(link);
+                outLinks[link] = o.var;
+            }
+        });
+        nodeObjs.push(nodeObj);
+    }
+
+    nodeObjs = topologicalSort(nodeObjs);
+    console.log(nodeObjs);
+    
+    for (const i in nodeObjs) {
+        console.log(i);
+        const node = nodeObjs[i];
+        const nodeType = node.type;
+        const args = [`'${nodeType}'`];
+        
+        // console.log(node);
+
+        Object.values(node.inputs).forEach(input => {
+            const def = `LINK_${input.type}_${input.link}`;
+            args.push(`${input.name}=${outLinks[input.link] ?? def}`)
+        });
+
+        Object.values(node.widgets).forEach(widget => {
+            const value = typeof widget.value === 'string' 
+                ? `r"${widget.value}"` 
+                : widget.value;
+            const uniqueName = widget.var;
+
+            vars[uniqueName] = value;
+            args.push(`${widget.name}=${uniqueName}`);
+        });
+
+        const nodeVar = node.var;
+        code.push(`# ${node.title}\n${nodeVar}=graph.node(${args.join(', ')})`);
+        for (let j in Object.values(node.outputs)) {
+            const output = Object.values(node.outputs)[j];
+
+            const links = output.links.map(
+                (link, index) => `${link}`
+            );
+            code.push(`${output.var}=${nodeVar}.out(${j}) # type: ${output.type}; links: ${links.join(', ')}`);
+        }
+        code.push('');
+    }
+
+    const varsText = Object.entries(vars)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('\n');
+    const codeStr = `${varsText}\n\n${code.join('\n')}`;
+
+    const newNode = app.graph.add(LiteGraph.createNode('PyExec_Output', 'PyExec_Output'));
+    const py = newNode.widgets.find((nw) => nw.name === 'py');
+    py.value = codeStr;
+
+    navigator.clipboard.writeText(codeStr)
+        .catch(err => console.error('Error:', err));
+
+    
+
+    
+    // for (const i in nodes) {
+    //     const node = nodes[i];
+    //     console.log(`${node.title}(${node.id})`);
+
+    //     node.inputs.forEach(input => 
+    //         console.log(`${input.name}=${input.link}`)
+    //     );
+
+    //     node.outputs.forEach(function (output) {
+    //             for (const j in output.links) {
+    //                 const link = output.links[j];
+    //                 console.log(`[${j}]=${link}`)
+    //             }
+    //         }
+    //     );
+    // }
 };
 
 app.registerExtension({
